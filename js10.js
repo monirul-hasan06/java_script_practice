08f1b59429299b5fd6f0b55c2e467211bcd82012
Monirul Hasan Mithu <monirul.hasan513@gmail.com>
2026-08-09 02:08:55 +0600

    added task01

---
+++ js10.js
@@ -0,0 +1,16 @@
+const books =['physics', 'chemestry', 'javascript', 'css'];
+    for(let i =0; i<books.length; i++){
+        console.log(books[i]);
+    }
+
+    const rev_books = books.reverse();
+    const sorted_books = books.sort(); 
+
+    // console.log(rev_books);
+    console.log(sorted_books);
+
+    const gedgets = [
+    {name: 'laptop', price: 20000, color: 'black', brand: 'lenovo'},
+    {name: 'phone', price: 10000, color: 'red', brand: 'sumsung'},
+    {name:'phone', price: 21000; color: 'blue', brand: 'realme'},
+    ];
